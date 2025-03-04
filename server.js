1ed7960d604caa0e7d8049d0ff701f221babbb34
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-03-05 01:40:07 +0530

    made register function for users

--- server.js
+++ server.js
@@ -3,15 +3,20 @@ const express = require(`express`);
 const app = express();
 const port = process.env.PORT || 3000;
 const connectDB = require(`./config/db`);
+const UserRoute = require(`./routes/userRoute`);
+const ProductRoute = require(`./routes/productRoute`);
+const CartRoute = require(`./routes/cartRoute`);
 
 
-
+// mongo db connection
 connectDB();
 
+app.use(express.json());
+// routes
 
-app.get(`/`,(req,res) => {
-    res.json("Hello from the server ")
-})
+app.use(`/api/auth/`, UserRoute);
+// app.use(`/api/products/`, ProductRoute);
+// app.use(`/api/cart/`, CartRoute);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
