b0b6b92fc77a48e0b1016b9c0ca5766eff21c89d
ASHUTOSH-SWAIN-GIT <ashutoshswain7383@gmail.com>
2025-03-05 01:08:08 +0530

    connected mongo db and made models

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ require(`dotenv`).config();
 const express = require(`express`);
 const app = express();
 const port = process.env.PORT || 3000;
+const connectDB = require(`./config/db`);
+
+
+
+connectDB();
 
 
 app.get(`/`,(req,res) => {
